00af05175e2a8aca3c1e7bf9992c401278709342
Juaj Xavier <jvitorxavier@gmail.com>
2021-02-26 17:02:25 -0300

    Migrate from create-react-app to Next.js

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+//aqui fica tudo que for repetir sempre/com frequência.
+import '../styles/global.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
