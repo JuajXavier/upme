7791968a0133dabc5e36a814b2647a139dc81196
Juaj Xavier <jvitorxavier@gmail.com>
2021-02-26 19:44:36 -0300

    Add countdown structure

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3e8debbd38ba3e466c49";
+/******/ 	var hotCurrentHash = "3a2bab0f64e86506e2f4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,11 +1,18 @@
+import styles from '../styles/components/Profile.module.css';
+
 export function Profile() {
     return (
-        <div>
+        <div className={styles.profileContainer}>
             <img src="https://github.com/juajxavier.png" alt="Juaj"/>
             <div>
                 <strong>Juaj Xavier</strong>
-                <p>Level 1</p>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level 1
+                </p>
             </div>
         </div>
     )
 }
+
+// todo arquivo do public já está público, não precisa de importação;
