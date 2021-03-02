34a3403488e198f240a524ab9000aaaaa5a5c9a6
Juaj Xavier <jvitorxavier@gmail.com>
2021-03-02 03:37:37 -0300

    Create challengeBox component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "72b9e0b12097ae1076e9";
+/******/ 	var hotCurrentHash = "c892c832a60fef4c24bc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
