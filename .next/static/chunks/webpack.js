cc8a185f8c753a223803bb859ff5ed0fe86ae23e
Juaj Xavier <jvitorxavier@gmail.com>
2021-03-29 16:12:38 -0300

    Create contexts folder

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cfbadb0d06e5692d145c";
+/******/ 	var hotCurrentHash = "b331f13fcb783ef728f2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
